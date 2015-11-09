0172665c6a716ad88450e6f52f92fe20ee6c9c65
Stephen Spann <stephenspann@gmail.com>
2015-11-09 17:22:26 -0500

    adding urls, 4th example

--- src/timer_impl.cpp
+++ src/timer_impl.cpp
@@ -49,7 +49,9 @@ namespace cpptimer {
      
     /*
      * Attempt 2: assign boost::asio::io_service::run() to its own thread
+     * from: http://stackoverflow.com/questions/28205052/boostdeadline-timerasync-wait-is-not-asynchronous
      */
+    
     
     TimerImpl::TimerImpl(const std::shared_ptr<TimerListener> & listener):
     io_service_(), timer_(io_service_, boost::posix_time::seconds(1)) {
@@ -64,7 +66,7 @@ namespace cpptimer {
         timer_.async_wait(boost::bind(&TimerImpl::TimerTick, this));
         
         // boost::asio::io_service::run() on its own thread
-        // th.join() still causes a block.
+        // th.join() still causes a block in the viewDidLoad() thread in Obj-C.
         std::thread th([&] { io_service_.run(); });
         th.join();
     }
@@ -80,6 +82,7 @@ namespace cpptimer {
             listener_->TimerEnded();
         }
     }
+
     
     /*
      * Attempt 3 (Broken): use boost::asio::io_service::post() with io_service in another thread
@@ -119,5 +122,39 @@ namespace cpptimer {
         }
     }
      */
+    
+    /*
+     * Attempt 4 (Broken): using a shared pointer for the io_service, then call boost::asio::io_service::post()
+     * http://stackoverflow.com/questions/13785640/using-boostasioio-servicepost
+     */
+    
+    /*
+    TimerImpl::TimerImpl(const std::shared_ptr<TimerListener> & listener):
+    io_service_(), timer_(io_service_, boost::posix_time::seconds(1)) {
+        listener_ = listener;
+        std::shared_ptr< boost::asio::io_service > io_service(new boost::asio::io_service);
+        io_service_ptr_ = io_service;
+    }
+    
+    void TimerImpl::StartTimer(int32_t seconds) {
+        
+        time_remaining_ = seconds;
+        
+        // Runtime Error: Called object type 'void (cpptimer::TimerImpl::*)()' is not a function or function pointer
+        io_service_ptr_->post(&TimerImpl::TimerTick);
+        
+    }
+    
+    void TimerImpl::TimerTick() {
+        time_remaining_--;
+        std:: cout << "C++: TimerTick() with " << std::to_string(time_remaining_) << " seconds remaining.\n";
+        if (time_remaining_ > 0) {
+            listener_->TimerTicked(time_remaining_);
+        } else {
+            listener_->TimerEnded();
+        }
+    }
+     */
+    
     
 }
