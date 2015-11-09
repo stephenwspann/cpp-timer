2deb2dc8e44872a0178ef033af105f57521804fc
Stephen Spann <stephenspann@gmail.com>
2015-11-09 18:50:03 -0500

    working asynchronous timer example, viewDidLoad no longer blocked

--- src/timer_impl.cpp
+++ src/timer_impl.cpp
@@ -1,7 +1,6 @@
 #include "timer_impl.hpp"
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/thread.hpp>
-#include <thread>
 
 namespace cpptimer {
     
@@ -52,7 +51,7 @@ namespace cpptimer {
      * from: http://stackoverflow.com/questions/28205052/boostdeadline-timerasync-wait-is-not-asynchronous
      */
     
-    
+    /*
     TimerImpl::TimerImpl(const std::shared_ptr<TimerListener> & listener):
     io_service_(), timer_(io_service_, boost::posix_time::seconds(1)) {
         listener_ = listener;
@@ -82,6 +81,7 @@ namespace cpptimer {
             listener_->TimerEnded();
         }
     }
+     */
 
     
     /*
@@ -155,6 +155,41 @@ namespace cpptimer {
         }
     }
      */
+    
+    
+    /*
+     * Attempt 5: after reading this ASIO tutorial
+     * (esp. how to properly use boost::bind)
+     * from: http://www.gamedev.net/blog/950/entry-2249317-a-guide-to-getting-started-with-boostasio/?pg=7
+     */
+    
+    TimerImpl::TimerImpl(const std::shared_ptr<TimerListener> & listener):
+            timer_(io_service_, boost::posix_time::seconds(1)) {
+        listener_ = listener;
+    }
+    
+    void TimerImpl::StartTimer(int32_t seconds) {
+        
+        time_remaining_ = seconds;
+        
+        io_service_.post(boost::bind(&TimerImpl::TimerTick, this));
+        
+        boost::thread th([&] { io_service_.run(); });
+
+    }
+    
+    void TimerImpl::TimerTick() {
+        time_remaining_--;
+        std:: cout << "C++: TimerTick() with " << std::to_string(time_remaining_) << " seconds remaining.\n";
+        if (time_remaining_ > 0) {
+            timer_.expires_from_now(boost::posix_time::seconds(1));
+            timer_.async_wait(boost::bind(&TimerImpl::TimerTick, this));
+            listener_->TimerTicked(time_remaining_);
+        } else {
+            listener_->TimerEnded();
+        }
+    }
+    
     
     
 }
